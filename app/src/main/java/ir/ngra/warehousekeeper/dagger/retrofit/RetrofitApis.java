d0a271a15385f7142fdea7ebed92ed1f6d9c176c
Mehrdad <m3.latifi@gmail.com>
2020-12-28 20:02:17 +0330

    دمو

--- app/src/main/java/ir/ngra/warehousekeeper/dagger/retrofit/RetrofitApis.java
+++ app/src/main/java/ir/ngra/warehousekeeper/dagger/retrofit/RetrofitApis.java
@@ -5,7 +5,8 @@ public class RetrofitApis {
     //    public static String Host = "http://185.159.87.205:70";
     //public static String Host = "https://ngra.bimija.com";
     //public static String Host = "http://192.168.1.110:70";
-    public static String Host = "http://samtam.app.ngraapp.ir";
+    public static String Host = "http://samtam.demo.ngraapp.ir";
+//    public static String Host = "http://samtam.app.ngraapp.ir";
 //    public static String Host = "http://bazyaft.app.ngraapp.ir";
 
 //    public static String client_id_value = "vRIzEFYjpzYwHHSUbx/ODg==";
